b0a3ea8cbe56b706ef21bc7e68732d7e63b2003a
Trinh Diep <trinhdiep.zoro@gmail.com>
2022-10-18 09:40:55 +0700

    update 18/10/2022

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from fileinput import close
-from logging import root
-from tkinter import *
-from tkinter import Label
 import sys
+from tkinter import *
+from tkinter import ttk, Label
+
 
 # import tkinter.messagebox
 # import random
@@ -15,53 +14,58 @@ class Window1:
     def __init__(self, master):
         # title and size+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         self.master = master
-        self.master.title("Drug Store Management System")
-        self.master.geometry('1400x700+0+0')
-        self.frame = Frame(self.master)
-        self.frame.grid()
+        self.master.title("Pharmacy Management Systems")
+        # self.master.config(bg='#105DC1')
+        # self.master.geometry('1400x700+0+0')
+        # self.frame = Frame(self.master)
+        # self.frame.grid(row=0, column=0, padx=0, pady=0)
+        self.frame = Frame(self.master, width=400, height=680)
+        self.frame.grid(row=0, column=0, padx=10)
 
+        self.rframe = Frame(self.master, width=980, height=680)
+        self.rframe.grid(row=0, column=1, padx=10, pady=10)
         # Buttons and Frames+++++++++++++++++++++++++++++++++++++++++++++++++++++++
-        # self.bg = PhotoImage(file='D:/New folder/bgggg.png')
-        # self.label = Label(master, image=self.bg)
-        # self.label.place(x=0, y=0)
+        self.bgf = PhotoImage(file='D:\GitHub\PharmacyManagementSystems_Python\gggg.png')
+        self.label = Label(self.frame, image=self.bgf)
+        self.label.grid(row=0, column=0)
 
-        self.LabelTitle = Label(self.frame, text="Drug Store Management System",bg="Blue", font=("Calibre", 40, 'bold'))
-        self.LabelTitle.grid(row=0, column=0, pady=0)
+        self.LabelTitle = Label(self.rframe, text="Pharmacy Management Systems", font=("Calibre", 30, 'bold'))
+        self.LabelTitle.grid(row=0, column=0, pady=10)
 
-        self.LabelTitle = Label(self.frame, text="________OR________", font=("Calibre", 20, 'bold'))
-        self.LabelTitle.grid(row=3, column=0, padx=600, pady=0)
+        self.LabelTitle = Label(self.rframe, text="________OR________", font=("Calibre", 15, 'bold'))
+        self.LabelTitle.grid(row=3, column=0)
         # Login information++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-        self.frame1 = Frame(self.frame, width=600, height=200, bd=10, relief='ridge')
-        self.frame1.grid(row=1, column=0, padx=400, pady=10)
+        self.frame1 = Frame(self.rframe, width=240, height=160, bd=10, relief='ridge')
+        self.frame1.grid(row=1, column=0, pady=10)
 
         self.lblUsername = Label(self.frame1, text="Username", font=("Calibre", 15, 'bold'), bd=10)
-        self.lblUsername.grid(row=0, column=0, pady=10)
-        self.txtUsername = Entry(self.frame1, font=("Calibre", 15, 'bold'), bd=10,
-                                 )
-        self.txtUsername.grid(row=0, column=1, pady=10)
+        self.lblUsername.grid(row=0, column=0)
+        self.txtUsername = Entry(self.frame1, font=("Calibre", 15, 'bold'), bd=10)
+        self.txtUsername.grid(row=0, column=1)
 
         self.lblPassword = Label(self.frame1, text="Password", font=("Calibre", 15, 'bold'), bd=10)
-        self.lblPassword.grid(row=1, column=0, pady=10)
-        self.txtPassword = Entry(self.frame1, font=("Calibre", 15, 'bold'), bd=10, show="*")
+        self.lblPassword.grid(row=1, column=0)
+        self.txtPassword = Entry(self.frame1, font=("Calibre", 15, 'bold'), show="*", bd=10)
         self.txtPassword.grid(row=1, column=1, pady=10)
         # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-        self.frame2 = Frame(self.frame, width=600, height=200, bd=10, relief='ridge')
-        self.frame2.grid(row=2, column=0, padx=650, pady=10)
+        self.frame2 = Frame(self.rframe, width=240, height=160, bd=10, relief='ridge')
+        self.frame2.grid(row=2, column=0, pady=10)
 
-        self.frame3 = Frame(self.frame, width=600, height=200, bd=10, relief='ridge')
-        self.frame3.grid(row=4, column=0, padx=650, pady=10)
+        self.frame3 = Frame(self.rframe, width=240, height=160, bd=10, relief='ridge')
+        self.frame3.grid(row=4, column=0, pady=10)
 
-        self.frame4 = Frame(self.frame, width=600, height=200, bd=10, relief='ridge')
-        self.frame4.grid(row=5, column=0, padx=650, pady=10)
+        self.frame4 = Frame(self.rframe, width=240, height=160, bd=10, relief='ridge')
+        self.frame4.grid(row=5, column=0, pady=10)
 
-        self.btnLogin = Button(self.frame2, text="Login", font=("Calibre", 20, 'bold'))
+        self.btnLogin = Button(self.frame2, text="Login", font=("Calibre", 18, 'bold'))
         self.btnLogin.grid(row=0, column=0)
 
-        self.btnRegistration = Button(self.frame3, text="Registration", font=("Calibre", 20, 'bold'), command=self.registration_window)
+        self.btnRegistration = Button(self.frame3, text="Registration", font=("Calibre", 18, 'bold'), command=self.registration_window)
         self.btnRegistration.grid(row=0, column=0)
 
-        self.btnExit = Button(self.frame4, text="Exit", font=("Calibre", 20, 'bold'),command=self.quit)
+        self.btnExit = Button(self.frame4, text="Exit", font=("Calibre", 15, 'bold'), command=self.quit)
         self.btnExit.grid(row=1, column=0)
+
     # registration windown
     def quit(self) :
         sys.exit()
@@ -74,7 +78,6 @@ class WindowR:
     def __int__(self, master):
         self.master = master
         self.master.title("Registration Systems")
-        self.master.geometry('1400x700+0+0')
         self.frame = Frame(self.master)
         self.frame.grid()
 
